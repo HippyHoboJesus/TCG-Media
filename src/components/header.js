0ba2f75f7ce892e4842caada6894dfa510466437
Jason Greer <jasemgreer@gmail.com>
2025-06-22 06:03:45 -0500

    1

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <div>
-            <div>Home</div>
+            <div></div>
             <div>Shop</div>
             <div>Profile</div>
         </div>
